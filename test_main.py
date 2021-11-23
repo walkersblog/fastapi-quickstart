c289a9fbe74757f4fe09e7117292a5369b6e44d9
Richard Walker <rwalker@redhat.com>
2021-11-23 14:35:26 +0000

    fix test

--- test_main.py
+++ test_main.py
@@ -8,4 +8,4 @@ client = TestClient(main_app)
 def test_read_main():
     response = client.get("/api/motd")
     assert response.status_code == 200
-    assert response.json() == {"message":"Hello FastAPI!!!"}
+    assert response.json() == {"message":"Hello FastAPI!"}
